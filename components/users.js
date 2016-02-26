dffb169635ba4d16f28c8c9a193acb6daec568bf
ace(sunpeng) <spirityy109@gmail.com>
2016-02-26 16:04:58 +0800

    add delete event

--- components/users.js
+++ components/users.js
@@ -1,12 +1,26 @@
 import React from 'react'
 
-//data
+//import simulate data
 import datausers from '../data/users.js'
 
 export default class Users extends React.Component {
   constructor() {
     super()
     this.state =  { datausers:datausers }
+  }
+  addUser() {
+    //add User
+
+  }
+  deleteUser(e) {
+    //delete User
+    const todel_user = this.state.datausers.find(function (data) {
+      return data.id === parseInt(e.target.value,10)
+    }.bind(e))
+    if (window.confirm('确定删除'+todel_user.name+'?')) {
+      this.state.datausers.splice(this.state.datausers.indexOf(todel_user),1)
+      this.setState({ datausers:this.state.datausers })
+    }
   }
   render() {
     return(
@@ -20,14 +34,15 @@ export default class Users extends React.Component {
                     <tr>
                       <th>id</th>
                       <th>name</th>
+                      <th>age</th>
                       <th>sex</th>
                       <th>operate</th>
                     </tr>
                   </thead>
                   <tbody>
                     {this.state.datausers.map(function (user,i) {
-                      return <User user={user} key={i} />
-                    })}
+                      return <User user={user} key={i} deleteUser={this.deleteUser.bind(this)}/>
+                    }.bind(this))}
                   </tbody>
                 </table>
               </div>
@@ -42,18 +57,14 @@ class User extends React.Component {
   constructor() {
     super()
   }
-  handleRemove(e) {
-    if (window.confirm('是否删除'+ this.props.user.name +'?')) {
-      alert(e.target.value);
-    }
-  }
   render() {
     return(
       <tr>
         <td>{this.props.user.id}</td>
         <td>{this.props.user.name}</td>
+        <td>{this.props.user.age}</td>
         <td>{this.props.user.sex}</td>
-        <td><button onClick={this.handleRemove.bind(this)} value={this.props.user.id}>delete</button></td>
+        <td><button onClick={this.props.deleteUser} value={this.props.user.id}>delete</button></td>
       </tr>
     )
   }
--- data/users.js
+++ data/users.js
@@ -2,26 +2,31 @@ const users = [
   {
     id:1,
     name:'tony',
+    age:20,
     sex:'male'
   },
   {
     id:2,
     name:'peter',
+    age:50,
     sex:'male'
   },
   {
     id:3,
     name:'jack',
+    age:40,
     sex:'male'
   },
   {
     id:4,
     name:'kate',
+    age:20,
     sex:'female'
   },
   {
     id:5,
     name:'rita',
+    age:30,
     sex:'female'
   }
 ]
