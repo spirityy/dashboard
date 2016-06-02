178bcce3c366831981f9a3d667e82dfd32961813
ace(sunpeng) <spirityy109@gmail.com>
2016-06-02 17:38:48 +0800

    add PureRenderMixin,immutablejs

--- components/orders.js
+++ components/orders.js
@@ -1,6 +1,27 @@
 import React from 'react'
+import Immutable from 'immutable'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
+
+//let da = { a:11, b:22, c:[ 1,2,3 ] }
 
 class Orders extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = Immutable.fromJS({
+      a:11,
+      b:22,
+      c:[ 1,2,3 ]
+    })
+    this.shouldComponentUpdate  = PureRenderMixin.shouldComponentUpdate.bind(this)
+    //this.state = da
+  }
+  shouldComponentUpdate() {
+    console.info(11)
+    return true
+  }
+  changeState() {
+    this.setState({ a:22 })
+  }
   render() {
     return(
       <main>
@@ -8,9 +29,8 @@ class Orders extends React.Component {
           <div className="users-wrapper">
               <h2>Orders</h2>
               <div className="main">
-                <table>
-                  table
-                </table>
+                <h3>{this.state.a}</h3>
+                <button onClick={this.changeState.bind(this)}>change state</button>
               </div>
           </div>
         </div>
