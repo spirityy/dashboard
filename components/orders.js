b577ac7c73b3c6e17b38bb5827ec6b0be1ca90c2
sunpeng <spirityy109@gmail.com>
2016-07-04 17:47:12 +0800

    add react-immutable-render-mixin and calc

--- components/orders.js
+++ components/orders.js
@@ -1,42 +1,60 @@
 import React from 'react'
 //import { Map,List } from 'immutable'
 import Immutable from 'immutable'
-import PureRenderMixin from 'react-addons-pure-render-mixin'
+//import PureRenderMixin from 'react-addons-pure-render-mixin'
+import { shouldComponentUpdate } from 'react-immutable-render-mixin'
+
 
 import '../assets/css/order.css'
 
 class Orders extends React.Component {
   constructor(props) {
     super(props)
-    /*
     this.state = {
-      orders:List([
-        Map({
-          a:11,
-          b:22,
-          c:[ 1,2,3 ]
-        })
+      orders:Immutable.fromJS([
+        {
+          id:1,
+          num:1,
+          price:11.3,
+          totalprice:11.3
+        },
+        {
+          id:2,
+          num:1,
+          price:15,
+          totalprice:15
+        }
       ])
     }
-    */
-    this.state = Immutable.fromJS({
-      orders:[
-        {
-          a:11,
-          b:22,
-          c:[ 1,2,3 ]
-        }
-      ]
-    })
-    this.shouldComponentUpdate  = PureRenderMixin.shouldComponentUpdate.bind(this)
-    //this.state = da
+    //this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this)
+    this.shouldComponentUpdate = shouldComponentUpdate.bind(this)
   }
-  changeState() {
-    this.setState(({ data }) => ({ data:data.update('a', v => v + 1) }))
-    console.info(this.state.data)
+  changeNum(i,method) {
+    //let order = this.state.get('orders').get(i)
+    //console.info(order)
+    let orders = this.state.orders
+    switch (method) {
+      case 'plus':
+        orders = (function () {
+          return orders.update(i,(order) => {
+            return Immutable.fromJS({ id:order.get('id'),num:parseInt(order.get('num')) + 1 ,price:order.get('price'),totalprice: parseFloat(order.get('price')) * (parseInt(order.get('num')) + 1) })
+          })
+        })()
+        this.setState({ orders })
+        break
+      case 'reduce':
+        orders = (function () {
+          return orders.update(i,(order) => {
+            return Immutable.fromJS({ id:order.get('id'),num:parseInt(order.get('num')) - 1 ,price:order.get('price'),totalprice: parseFloat(order.get('price')) * (parseInt(order.get('num')) - 1) })
+          })
+        })()
+        this.setState({ orders })
+        break
+    }
   }
   render() {
-    let orders = this.state.get('orders')
+    console.info(111)
+    let orders = this.state.orders
     return(
       <main>
         <div className="main-wrapper">
@@ -49,16 +67,22 @@ class Orders extends React.Component {
                       <th>id</th>
                       <th>num</th>
                       <th>price</th>
+                      <th>totalprice</th>
                       <th>time</th>
                     </tr>
                   </thead>
                   <tbody>
                     {orders.map(function (order,i) {
                       return (<tr key={i}>
-                        <td>{order.get('a')}</td>
-                        <td>{order.get('b')}</td>
-                        <td>{order.get('c')}</td>
-                        <td>3213</td>
+                        <td>{order.get('id')}</td>
+                        <td>
+                          <i onClick={this.changeNum.bind(this,i,'reduce')}>-</i>
+                          {order.get('num')}
+                          <i onClick={this.changeNum.bind(this,i,'plus')}>+</i>
+                        </td>
+                        <td>{order.get('price')}</td>
+                        <td>{order.get('totalprice')}</td>
+                        <td>2015-03-09</td>
                       </tr>)
                     }.bind(this))}
                   </tbody>
